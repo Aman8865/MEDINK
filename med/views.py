989d9417b759e7ee8c49b681778186bdfdfeab2b
Aman8865 <syalaman31@gmail.com>
2025-10-07 11:17:31 +0530

    Initial commit

---
+++ med/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def login(request):
+    return render(request, 'login.html')
+
+def index(request):
+    return render(request, 'index.html')
+    
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def signup(request):
+    return render(request,'signup.html')
+
+def popupform(request):
+    return render(request,'popupform.html')
+def report(request, id):
+    return render(request, 'report.html', {"id": id})
+
+
+def user_detail(request):
+    return render(request, 'user_detail.html')
+
+def imagingA(request):
+    return render(request, 'imagingA.html')
---
+++ medink/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for medink project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from med.views import *
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',login,name='login'),
+    # path('',home,name='home'),
+    path('index/', index, name='index'),
+    path('profile/',profile,name='profile'),
+    path('signup/',signup,name='signup'),
+    path('popupform/',popupform,name='popupform'),
+    path('report/<int:id>/',report,name='report'),
+    path('user_detail/',user_detail,name='user_detail'),
+    path('imagingA/',imagingA,name='imagingA'),
+    
+]
